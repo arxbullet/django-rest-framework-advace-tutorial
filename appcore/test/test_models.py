b9ff7c7feedcd6c0ba77caee2d8bd7505e071f5a
arx_bullet <aranzav.c.b@gmail.com>
2022-05-06 22:56:57 -0400

    new test module for Test-Driven Development

---
+++ appcore/test/test_models.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTest(TestCase):
+    """probar crear un nuevo usuario con un email correctamente"""
+    
