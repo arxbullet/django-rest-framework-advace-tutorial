e7f1fb3e9f203dbbab0173c345e8a9cbf98da3a9
arx_bullet <aranzav.c.b@gmail.com>
2022-05-07 00:58:20 -0400

    added first test to desing coding

--- appcore/test/test_models.py
+++ appcore/test/test_models.py
@@ -3,4 +3,13 @@ from django.contrib.auth import get_user_model
 
 class ModelTest(TestCase):
     """probar crear un nuevo usuario con un email correctamente"""
-    
+    def test_create_user_with_email_succesfull(self):
+        email = 'test@mail.com'
+        password = 'test123'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
