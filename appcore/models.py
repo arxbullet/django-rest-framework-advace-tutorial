5644305344fd8da589c41e510a05840e2e338a9e
arx_bullet <aranzav.c.b@gmail.com>
2022-05-11 13:52:14 -0400

    added user manager and user models

--- appcore/models.py
+++ appcore/models.py
@@ -6,10 +6,24 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager (BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """crea Guardo un nueve Usuarlo """
-        user = self.model(email=email, **extra_fields)
-        user.setpassword(password)
+        if not email: #value error for test invalid mail
+            raise ValueError('user mst have an email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password):
+        """ Crear super usuario"""
+        user = self.create_user(email, password)
+        user.is_staf = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+    
+
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     
